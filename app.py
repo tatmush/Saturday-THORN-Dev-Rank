d413984be36b9719297012d53ce56e453bbef2bc
tatmush <tatalmondmush@gmail.com>
2019-03-12 16:54:59 +0200

    disable firebase sdk from backend

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-import firebase_admin
-from firebase_admin import credentials, auth
+#import firebase_admin
+#from firebase_admin import credentials, auth
 
 app = Flask(__name__)
 
